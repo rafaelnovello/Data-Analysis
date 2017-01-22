d3a909ef329675c0f707c374eb2f0f33a64ae846
Rafael Novello <rafa.reis.novello@gmail.com>
2017-01-21 23:06:24 -0200

    Adicionando artigos do Estadão e Goose como dependencia.

--- G1_categorizer.py
+++ G1_categorizer.py
@@ -6,6 +6,7 @@ import logging
 import argparse
 import unicodedata
 
+from goose import Goose
 from pandas import DataFrame
 from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
@@ -18,23 +19,51 @@ Editorials = (
     ('http://g1.globo.com/educacao/', 'educacao'),
     ('http://g1.globo.com/politica/', 'politica'),
     ('http://g1.globo.com/tecnologia/', 'tecnologia'),
+    ('http://politica.estadao.com.br/', 'politica'),
+    ('http://economia.estadao.com.br/', 'economia'),
+    ('http://educacao.estadao.com.br/', 'educacao'),
+    ('http://ciencia.estadao.com.br/', 'ciencia-e-saude'),
+    ('http://saude.estadao.com.br/', 'ciencia-e-saude')
 )
 
+
+def to_unicode(data):
+    try:
+        data = data.decode('utf-8')
+    except (UnicodeDecodeError, UnicodeEncodeError):
+        try:
+            data = data.decode('iso-8859-1')
+        except (UnicodeDecodeError, UnicodeEncodeError):
+            try:
+                data = data.decode('latin-1')
+            except (UnicodeDecodeError, UnicodeEncodeError):
+                data = data
+    return data
+
+
 def remove_nonlatin(s): 
-    s = (ch for ch in s
-         if unicodedata.name(ch).startswith(('LATIN', 'SPACE')))
-    return ''.join(s)
+    # s = (ch for ch in s
+    #      if unicodedata.name(unicode(ch)).startswith(('LATIN', 'SPACE')))
+    ss = []
+    for ch in s:
+        if ch == '\n':
+            ss.append(' ')
+            continue
+        try:
+            if unicodedata.name(unicode(ch)).startswith(('LATIN', 'SPACE')):
+                ss.append(ch)
+        except:
+            continue
+    return ''.join(ss)
 
 def worker(link, categ, lines):
     stops = set(stopwords.words("portuguese"))
-    html = requests.get(link).content
-    html = BeautifulSoup(html, 'lxml')
-    matter = html.find_all('div', 'content-text')
-    if not matter:
-        return
-    text = ' '.join([ch.get_text() for ch in matter])
-    text = remove_nonlatin(text)
+    goose = Goose()
+    article = goose.extract(link)
+    text = article.cleaned_text
+    text = remove_nonlatin(to_unicode(text))
     words = text.lower().split()
+    print len(words)
     words = ' '.join([w for w in words if not w in stops])
     lines.append((link, categ, words))
 
@@ -46,6 +75,7 @@ def main():
         resp = requests.get(edit)
         html = BeautifulSoup(resp.content, 'lxml')
         posts = html.find_all('a', 'feed-post-link')
+        posts = html.find_all('a', 'link-title') if not posts else posts
 
         for post in posts:
             link = post.get('href')
@@ -54,6 +84,7 @@ def main():
     pool.shutdown(wait=True)
     df = DataFrame(lines)
     df.to_csv('bag_words.csv', index=False, encoding='utf-8', sep=';')
+
 
 if __name__ == '__main__':
 
